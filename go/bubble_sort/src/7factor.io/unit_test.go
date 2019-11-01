a551bf79871a701ff4c70b8d883190d5b7c2b3e6
chris-malloy <chrismalloymusic@gmail.com>
2019-11-01 08:51:16 -0400

    Passing single element test.

--- go/bubble_sort/src/7factor.io/unit_test.go
+++ go/bubble_sort/src/7factor.io/unit_test.go
@@ -21,11 +21,20 @@ var _ = Describe("Given an array of ints", func() {
 		})
 	})
 
-	Context("When pass an empty array", func() {
+	Context("When passed an empty array", func() {
 		It("Should return an empty array and no errors", func() {
 			emptyArr := make([]int, 0)
 			actual, err := bubbleSort(emptyArr)
 			Expect(actual).To(Equal(emptyArr))
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Context("When passed an array with a single element", func() {
+		It("Should return the array and no errors", func() {
+			singleElement := []int{1}
+			actual, err := bubbleSort(singleElement)
+			Expect(actual).To(Equal(actual))
 			Expect(err).To(BeNil())
 		})
 	})
