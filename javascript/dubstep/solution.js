9748b66286bd94da3065946437f96584674e2609
chris-malloy <chrismalloymusic@gmail.com>
2020-01-04 11:27:35 -0500

    Handing lowercase and whitespace inputs.

--- javascript/dubstep/solution.js
+++ javascript/dubstep/solution.js
@@ -1,8 +1,27 @@
 let decode = (song) => {
-  if (song === null) {
-    return 'null is not allowed';
+  let err = _catchErrors(song);
+  if (err !== null) {
+    return err;
   }
+
   return null
+};
+
+let _catchErrors = (input) => {
+  let lowerCaseRe = new RegExp('[a-z]');
+  let spaceRe = new RegExp('[\\s]');
+
+  if (input === null) {
+    return new Error('null is not allowed')
+  }
+
+  if (input.match(lowerCaseRe)) {
+    return new Error('lower case letters are not allowed')
+  }
+
+  if (input.match(spaceRe)) {
+    return new Error('no whitespace characters allowed')
+  }
 };
 
 module.exports = {
--- javascript/dubstep/test.js
+++ javascript/dubstep/test.js
@@ -3,6 +3,16 @@ const songDecoder = require('./solution');
 describe('Dubstep', () => {
   test('should not accept a null input', () => {
     let actual = songDecoder.decode(null);
-    expect(actual).toEqual('null is not allowed');
+    expect(actual).toEqual(new Error('null is not allowed'))
   });
+
+  test('should not accept lowercase letters in input', () => {
+    let actual = songDecoder.decode('somelowercaseletters');
+    expect(actual).toEqual(new Error('lower case letters are not allowed'))
+  });
+
+  test('should not accept whitespace characters in input', () => {
+    let actual = songDecoder.decode('  WHITESPACE   ');
+    expect(actual).toEqual(new Error('no whitespace characters allowed'))
+  })
 });
