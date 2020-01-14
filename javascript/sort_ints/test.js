8c923c39185001bbe9b5ea52742b02bf3d6da08b
chris-malloy <chrismalloymusic@gmail.com>
2020-01-14 00:13:17 -0500

    Passing first unsort three element array test.

--- javascript/sort_ints/test.js
+++ javascript/sort_ints/test.js
@@ -2,33 +2,35 @@ const sorter = require('./solution');
 
 describe('The Integer Sorting Kata', () => {
   test('should return empty array with null input', () => {
-    let actual = sorter.sort(null);
-    expect(actual).toEqual([]);
+    _runSort(null, []);
   });
 
   test('should return empty array with empty array input', () => {
-    let actual = sorter.sort([]);
-    expect(actual).toEqual([]);
+    _runSort([], [])
   });
 
   test('should return the array with single item array input', () => {
-    let actual = sorter.sort([1]);
-    expect(actual).toEqual([1]);
+    _runSort([1], [1]);
   });
 
   test('should return the array with a sorted, two item array input', () => {
-    let actual = sorter.sort([1,2]);
-    expect(actual).toEqual([1,2]);
+    _runSort([1,2], [1,2]);
   });
 
   test('should return a sorted array with an unsorted, two item array input', () => {
-    let actual = sorter.sort([2,1]);
-    expect(actual).toEqual([1,2]);
+    _runSort([2,1], [1,2]);
   });
 
   test('should return the array with a sorted, three item array input', () => {
-    let actual = sorter.sort([1,2,3]);
-    expect(actual).toEqual([1,2,3]);
+    _runSort([1,2,3], [1,2,3]);
   });
 
+  test('should return the array with the first element of a three item array out of order', () => {
+    _runSort([2,1,3], [1,2,3]);
+  });
 });
+
+let _runSort = (input, expected) => {
+  let actual = sorter.sort(input);
+  expect(actual).toEqual(expected);
+};
