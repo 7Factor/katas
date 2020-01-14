4f51680c343c5464adbc878593a755443476cbcf
chris-malloy <chrismalloymusic@gmail.com>
2020-01-14 00:05:02 -0500

    Refactoring.

--- javascript/sort_ints/solution.js
+++ javascript/sort_ints/solution.js
@@ -1,26 +1,43 @@
 let sort = (arr) => {
-    if (arr == null) {
-        return [];
-    }
 
-    if (arr.length < 2) {
-        return arr;
+    let cleaned = _clean(arr);
+    if (cleaned.length < 2) {
+        return cleaned;
     }
 
     let separator = arr[0];
     let lessThan = [];
     let greaterThan = [];
-    arr.forEach((int) => {
-        if (int < separator) {
-            lessThan.push(int);
-        }
-        if (int > separator) {
-            greaterThan.push(int)
-        }
+    cleaned.forEach((int) => {
+        lessThan = buildLessThan(lessThan, separator, int);
+        greaterThan = buildGreaterThan(greaterThan, separator, int);
     });
 
     return lessThan.concat(separator).concat(greaterThan);
 };
+
+let buildLessThan = (arr, separator, int) => {
+    if (int < separator) {
+        arr.push(int);
+    }
+    return arr;
+};
+
+let buildGreaterThan = (arr, separator, int) => {
+    if (int > separator) {
+        arr.push(int)
+    }
+    return arr;
+};
+
+let _clean = (arr) => {
+    if (arr == null) {
+        return [];
+    }
+
+    return arr;
+};
+
 
 module.exports = {
     sort
