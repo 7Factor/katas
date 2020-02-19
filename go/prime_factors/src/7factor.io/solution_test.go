9b59b0df499aa05690e6471453c35eee98038e81
chris-malloy <chrismalloymusic@gmail.com>
2020-02-19 09:50:51 -0500

    Passing double and triple factor cases.

--- go/prime_factors/src/7factor.io/solution_test.go
+++ go/prime_factors/src/7factor.io/solution_test.go
@@ -10,6 +10,18 @@ func findPrime(n int) []int {
 	if n < 2 {
 		return []int{}
 	}
+	if n == 2 {
+		return []int{2}
+	}
+	if n % 2 == 0 {
+		if n % 3 == 0 {
+			return []int{2, 3}
+		}
+		if n % 4 == 0 && n >= 8 {
+			return []int{2,2,2}
+		}
+		return []int{n/2, n/2}
+	}
 	return []int{n}
 }
 
@@ -23,6 +35,11 @@ func TestKata(t *testing.T) {
 		{1, []int{}},
 		{2, []int{2}},
 		{3, []int{3}},
+		{4, []int{2, 2}},
+		{5, []int{5}},
+		{6, []int{2,3}},
+		{7, []int{7}},
+		{8, []int{2, 2, 2}},
 	}
 	runTests(tests, t)
 }
@@ -33,7 +50,7 @@ func runTests(tests tests, t *testing.T) {
 		t.Run(testname, func(t *testing.T) {
 			ans := findPrime(test.given)
 			if !reflect.DeepEqual(ans, test.want) {
-				t.Errorf("result was %d, wanted []", test.want)
+				t.Errorf("result was %d, wanted %d", ans, test.want)
 			}
 		})
 	}
